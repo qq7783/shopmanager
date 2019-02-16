37052540f45accec6e1e8e0b4d5d40cc94109180
gb <77834992@qq.com>
2019-02-16 20:53:16 +0800

    引入登录组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>login--</div>
+  <div class="login-wrap">
+    <el-form label-position="top" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
