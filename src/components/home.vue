d677bab36d89d781403ba0c0196134be298d51a9
gb <77834992@qq.com>
2019-02-18 11:26:00 +0800

    home头部样式完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header class="top">Header</el-header>
+    <el-header class="top">
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="图片加载失败" />
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
 
     <el-container>
       <el-aside class="left" width="200px">Aside</el-aside>
@@ -23,5 +35,14 @@ export default {};
 }
 .main {
   background: pink;
+}
+.middle {
+    line-height: 60px;
+    text-align: center;
+    color: #fff
+}
+.logout {
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
